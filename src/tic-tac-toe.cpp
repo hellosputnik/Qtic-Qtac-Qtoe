b4bf488cb0fda3359c9222caef673c8b7142354b
hellosputnik <hallosputnik@gmail.com>
2017-05-13 16:48:22 -0700

    Add game logic implementation
     * Added logic to start a new game
     * Added methods for changing turns and checking whose turn
     * Added method to check for a victory

--- src/tic-tac-toe.cpp
+++ src/tic-tac-toe.cpp
@@ -1,28 +1,92 @@
+// Qt headers
+#include <QDebug>
+
+// STL headers
+#include <algorithm>
+
 #include "tic-tac-toe.h"
 
-TicTacToe::TicTacToe(QPushButton *new_game_button, GameBoard *board)
+TicTacToe::TicTacToe()
 {
-    this->new_game_button_ = new_game_button;
-    this->board_           = board;
-    this->turn_            = 0;
+    new_game_button_ = nullptr;
+    board_           = nullptr;
+    turn_            = 0;
+    players_[0]      = 'O';
+    players_[1]      = 'X';
+}
+
+TicTacToe::TicTacToe(QPushButton *new_game_button, GameBoard* board)
+{
+    new_game_button_ = new_game_button;
+    board_           = board;
+    turn_            = 0;
+    players_[0]      = 'O';
+    players_[1]      = 'X';
+}
+
+TicTacToe::~TicTacToe()
+{
+    delete board_;
 }
 
 void TicTacToe::ChangeTurn()
 {
-    this->turn_ ^= 1;
+    // XOR between player 0 and player 1.
+    turn_ ^= 1;
 }
 
 bool TicTacToe::CheckWinCondition()
 {
-    // Check horizontally.
-    // Check vertically.
-    // Check diagonally.
+    // Check if there is a row that belongs to a single player.
+    if (board_->northwest->text() == board_->north->text() &&
+        board_->north->text() == board_->northeast->text())
+        return true;
+    if (board_->west->text() == board_->center->text() &&
+        board_->center->text() == board_->east->text())
+        return true;
+    if (board_->southwest->text() == board_->south->text() &&
+        board_->south->text() == board_->southeast->text())
+        return true;
+
+    // Check if there is a column that belongs to a single player.
+    if (board_->northwest->text() == board_->west->text() &&
+        board_->west->text() == board_->southwest->text())
+        return true;
+    if (board_->north->text() == board_->center->text() &&
+        board_->center->text() == board_->south->text())
+        return true;
+    if (board_->northeast->text() == board_->east->text() &&
+        board_->east->text() == board_->southeast->text())
+        return true;
+
+    // Check if a single player diagonally owns board spaces.
+    if (board_->northwest->text() == board_->center->text() &&
+        board_->center->text() == board_->southeast->text())
+        return true;
+    if (board_->northeast->text() == board_->center->text() &&
+        board_->center->text() == board_->southwest->text())
+        return true;
+
+    return false;
+}
+
+QChar TicTacToe::GetCurrentPlayer()
+{
+    // Based on the turn, return the player's character.
+    return players_[turn_];
 }
 
 void TicTacToe::NewGame()
 {
     // Disable the new_game button.
-    // Clear the game board.
-    // Enable the spaces to be clicked.
-}
+    new_game_button_->setEnabled(false);
 
+    // Clear the game board by enabling the button and clearing its text.
+    auto clear = [] (QPushButton* button) {
+        button->setEnabled(true);
+        button->setText(" ");
+    };
+
+    // Map the clear function to all the QPushButtons.
+    std::for_each(board_->all.begin(), board_->all.end(), clear);
+}
--- src/tic-tac-toe.h
+++ src/tic-tac-toe.h
@@ -9,19 +9,18 @@
 
 struct GameBoard
 {
-    QPushButton* northwest;
-    QPushButton* north;
-    QPushButton* northeast;
-    QPushButton* west;
-    QPushButton* center;
-    QPushButton* east;
-    QPushButton* southwest;
-    QPushButton* south;
-    QPushButton* southeast;
+    QPushButton* northwest; QPushButton* north;  QPushButton* northeast;
+    QPushButton* west;      QPushButton* center; QPushButton* east;
+    QPushButton* southwest; QPushButton* south;  QPushButton* southeast;
 
-    std::vector<QPushButton*> all = { northwest, north,  northeast,
-                                      west,      center, east,
-                                      southwest, south,  southeast };
+    std::vector<QPushButton*> all;
+
+    void Initialize()
+    {
+        all = { northwest, north,  northeast,
+                west,      center, east,
+                southwest, south,  southeast };
+    }
 };
 
 class TicTacToe
@@ -29,17 +28,24 @@ class TicTacToe
 
 public:
 
+    // Tic-Tac-Toe constructors and destructors
+    TicTacToe();
     TicTacToe(QPushButton* new_game_button, GameBoard* board);
+    ~TicTacToe();
 
-    void ChangeTurn();
-    bool CheckWinCondition();
-    void NewGame();
+    // Tic-Tac-Toe methods
+    void  ChangeTurn();
+    bool  CheckWinCondition();
+    QChar GetCurrentPlayer();
+    void  NewGame();
 
 private:
 
+    // Tic-Tac-Toe data members
     QPushButton* new_game_button_;
     GameBoard*   board_;
     int          turn_;
+    QChar        players_[2];
 
 };
 
